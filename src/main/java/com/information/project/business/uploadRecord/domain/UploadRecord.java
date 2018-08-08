99c0d73d4ab37d9e3d0392f29ff15afea69f79aa
刘宁 <z517415323z@qq.com>
2018-08-08 20:13:26 +0800

    文件上传记录增删改

---
+++ src/main/java/com/information/project/business/uploadRecord/domain/UploadRecord.java
@@ -0,0 +1,233 @@
+package com.information.project.business.uploadRecord.domain;
+
+import com.information.framework.web.domain.BaseEntity;
+import java.util.Date;
+
+/**
+ * 功能导入记录表 bus_upload_record
+ * 
+ * @author LiuNing
+ * @date 2018-08-08
+ */
+public class UploadRecord extends BaseEntity
+{
+	private static final long serialVersionUID = 1L;
+	
+	/** 主键 */
+	private Long id;
+	/** 功能模块名称 */
+	private String module;
+	/** 上传文件名 */
+	private String uploadName;
+	/** 成功条数 */
+	private Long successCount;
+	/** 失败文件名 */
+	private String failName;
+	/** 失败条数 */
+	private Long failCount;
+	/** 状态 */
+	private String status;
+	/** 备注 */
+	private String remark;
+	/** 创建时间 */
+	private Date createTime;
+	/** 创建人 */
+	private String createBy;
+	/** 修改时间 */
+	private Date updateTime;
+	/** 修改人 */
+	private String updateBy;
+
+	/**
+	 * 设置：主键
+	 */
+	public void setId(Long id) 
+	{
+		this.id = id;
+	}
+	
+	/**
+	 * 获取：主键
+	 */
+	public Long getId() 
+	{
+		return id;
+	}
+	
+	/**
+	 * 设置：功能模块名称
+	 */
+	public void setModule(String module) 
+	{
+		this.module = module;
+	}
+	
+	/**
+	 * 获取：功能模块名称
+	 */
+	public String getModule() 
+	{
+		return module;
+	}
+	
+	/**
+	 * 设置：上传文件名
+	 */
+	public void setUploadName(String uploadName) 
+	{
+		this.uploadName = uploadName;
+	}
+	
+	/**
+	 * 获取：上传文件名
+	 */
+	public String getUploadName() 
+	{
+		return uploadName;
+	}
+	
+	/**
+	 * 设置：成功条数
+	 */
+	public void setSuccessCount(Long successCount) 
+	{
+		this.successCount = successCount;
+	}
+	
+	/**
+	 * 获取：成功条数
+	 */
+	public Long getSuccessCount() 
+	{
+		return successCount;
+	}
+	
+	/**
+	 * 设置：失败文件名
+	 */
+	public void setFailName(String failName) 
+	{
+		this.failName = failName;
+	}
+	
+	/**
+	 * 获取：失败文件名
+	 */
+	public String getFailName() 
+	{
+		return failName;
+	}
+	
+	/**
+	 * 设置：失败条数
+	 */
+	public void setFailCount(Long failCount) 
+	{
+		this.failCount = failCount;
+	}
+	
+	/**
+	 * 获取：失败条数
+	 */
+	public Long getFailCount() 
+	{
+		return failCount;
+	}
+	
+	/**
+	 * 设置：状态
+	 */
+	public void setStatus(String status) 
+	{
+		this.status = status;
+	}
+	
+	/**
+	 * 获取：状态
+	 */
+	public String getStatus() 
+	{
+		return status;
+	}
+	
+	/**
+	 * 设置：备注
+	 */
+	public void setRemark(String remark) 
+	{
+		this.remark = remark;
+	}
+	
+	/**
+	 * 获取：备注
+	 */
+	public String getRemark() 
+	{
+		return remark;
+	}
+	
+	/**
+	 * 设置：创建时间
+	 */
+	public void setCreateTime(Date createTime) 
+	{
+		this.createTime = createTime;
+	}
+	
+	/**
+	 * 获取：创建时间
+	 */
+	public Date getCreateTime() 
+	{
+		return createTime;
+	}
+	
+	/**
+	 * 设置：创建人
+	 */
+	public void setCreateBy(String createBy) 
+	{
+		this.createBy = createBy;
+	}
+	
+	/**
+	 * 获取：创建人
+	 */
+	public String getCreateBy() 
+	{
+		return createBy;
+	}
+	
+	/**
+	 * 设置：修改时间
+	 */
+	public void setUpdateTime(Date updateTime) 
+	{
+		this.updateTime = updateTime;
+	}
+	
+	/**
+	 * 获取：修改时间
+	 */
+	public Date getUpdateTime() 
+	{
+		return updateTime;
+	}
+	
+	/**
+	 * 设置：修改人
+	 */
+	public void setUpdateBy(String updateBy) 
+	{
+		this.updateBy = updateBy;
+	}
+	
+	/**
+	 * 获取：修改人
+	 */
+	public String getUpdateBy() 
+	{
+		return updateBy;
+	}
+	
+}
---
+++ src/main/java/com/information/project/business/uploadRecord/service/UploadRecordServiceImpl.java
@@ -0,0 +1,104 @@
+package com.information.project.business.uploadRecord.service;
+
+import com.information.common.constant.Constants;
+import com.information.common.utils.security.ShiroUtils;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.information.project.business.uploadRecord.mapper.UploadRecordMapper;
+import com.information.project.business.uploadRecord.domain.UploadRecord;
+import com.information.project.business.uploadRecord.service.IUploadRecordService;
+import com.information.common.support.Convert;
+
+/**
+ * 功能导入记录 服务层实现
+ * 
+ * @author LiuNing
+ * @date 2018-08-08
+ */
+@Service
+public class UploadRecordServiceImpl implements IUploadRecordService 
+{
+	@Autowired
+	private UploadRecordMapper uploadRecordMapper;
+
+	/**
+     * 查询功能导入记录信息
+     * 
+     * @param id 功能导入记录ID
+     * @return 功能导入记录信息
+     */
+    @Override
+	public UploadRecord selectUploadRecordById(Long id)
+	{
+	    return uploadRecordMapper.selectUploadRecordById(id);
+	}
+	
+	/**
+     * 查询功能导入记录列表
+     * 
+     * @param uploadRecord 功能导入记录信息
+     * @return 功能导入记录集合
+     */
+	@Override
+	public List<UploadRecord> selectUploadRecordList(UploadRecord uploadRecord)
+	{
+	    return uploadRecordMapper.selectUploadRecordList(uploadRecord);
+	}
+	
+    /**
+     * 新增功能导入记录
+     * 
+     * @param uploadRecord 功能导入记录信息
+     * @return 结果
+     */
+	@Override
+	public int insertUploadRecord(UploadRecord uploadRecord)
+	{
+	    uploadRecord.setStatus(Constants.STATUS_ACTIVE);
+
+        uploadRecord.setCreateBy(ShiroUtils.getUserId().toString());
+	    return uploadRecordMapper.insertUploadRecord(uploadRecord);
+	}
+	
+	/**
+     * 修改功能导入记录
+     * 
+     * @param uploadRecord 功能导入记录信息
+     * @return 结果
+     */
+	@Override
+	public int updateUploadRecord(UploadRecord uploadRecord)
+	{
+	    uploadRecord.setUpdateBy(ShiroUtils.getUserId().toString());
+	    return uploadRecordMapper.updateUploadRecord(uploadRecord);
+	}
+
+	/**
+     * 删除功能导入记录对象
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+	@Override
+	public int deleteUploadRecordByIds(String ids)
+	{
+	    String [] idsArray = Convert.toStrArray(ids);
+        for (String id: idsArray) {
+
+            UploadRecord uploadRecord = new UploadRecord();
+            uploadRecord.setId(Long.valueOf(id));
+            //初始化數據信息
+
+            uploadRecord.setStatus(Constants.STATUS_REMOVED);
+
+            uploadRecord.setUpdateBy(ShiroUtils.getUserId().toString());
+
+            uploadRecordMapper.updateUploadRecord(uploadRecord);
+
+        }
+
+        return 1;
+	}
+	
+}
