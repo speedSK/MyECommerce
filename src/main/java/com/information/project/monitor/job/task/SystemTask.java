5d1eee0ee3dc2b8d8129a06f98a19e5fc1b8e4c6
刘宁 <z517415323z@qq.com>
2018-08-13 19:29:51 +0800

    定时任务调试

--- src/main/java/com/information/project/monitor/job/task/SystemTask.java
+++ src/main/java/com/information/project/monitor/job/task/SystemTask.java
@@ -1,6 +1,12 @@
 package com.information.project.monitor.job.task;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.information.project.business.transactionRecord.domain.TransactionRecord;
+import com.information.project.business.transactionRecord.service.ITransactionRecordService;
 
 /**
  * 定时任务调度测试
@@ -10,7 +16,9 @@ import org.springframework.stereotype.Component;
 @Component("systemTask")
 public class SystemTask
 {
-
+	@Autowired
+	private ITransactionRecordService transactionRecordService;
+	
     public void ryParams(String params)
     {
         System.out.println("执行有参方法：" + params);
@@ -18,7 +26,8 @@ public class SystemTask
 
     public void ryNoParams()
     {
-        System.out.println("执行无参方法");
+    	List<TransactionRecord> list = transactionRecordService.selectTransactionRecordList(new TransactionRecord());
+    	System.out.println(list.size());
     }
 
 }
