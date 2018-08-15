69ed1daef1486559667adb960816c05e2f794572
刘宁 <z517415323z@qq.com>
2018-08-15 15:00:22 +0800

    重新生成报表相关代码

--- src/main/java/com/information/project/business/operReport/controller/OperReportController.java
+++ src/main/java/com/information/project/business/operReport/controller/OperReportController.java
@@ -22,7 +22,7 @@ import com.information.framework.web.domain.AjaxResult;
  * 系统操作报 信息操作处理
  * 
  * @author LiuNing
- * @date 2018-08-06
+ * @date 2018-08-15
  */
 @Controller
 @RequestMapping("/business/operReport")
--- src/main/java/com/information/project/business/operReport/domain/OperReport.java
+++ src/main/java/com/information/project/business/operReport/domain/OperReport.java
@@ -1,14 +1,17 @@
 package com.information.project.business.operReport.domain;
 
-import java.math.BigDecimal;
-
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import com.information.framework.web.domain.BaseEntity;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 系统操作报表 bus_oper_report
  * 
  * @author LiuNing
- * @date 2018-08-06
+ * @date 2018-08-15
  */
 public class OperReport extends BaseEntity
 {
@@ -17,92 +20,115 @@ public class OperReport extends BaseEntity
 	/**  */
 	private Long id;
 	/** 报表日期 */
-	private String reportDate;
+	private Date reportDate;
 	/** 交易码 */
 	private String tradeCode;
 	/** 交易笔数 */
 	private Integer tradeNum;
 	/** 交易金额 */
 	private BigDecimal tradeSum;
+	/**  */
+	private String remark;
+	/**  */
+	private String status;
+	/**  */
+	private Date createTime;
+	/**  */
+	private Date updateTime;
 
-	/**
-	 * 设置：
-	 */
 	public void setId(Long id) 
 	{
 		this.id = id;
 	}
-	
-	/**
-	 * 获取：
-	 */
+
 	public Long getId() 
 	{
 		return id;
 	}
-	
-	/**
-	 * 设置：报表日期
-	 */
-	public void setReportDate(String reportDate) 
+	public void setReportDate(Date reportDate) 
 	{
 		this.reportDate = reportDate;
 	}
-	
-	/**
-	 * 获取：报表日期
-	 */
-	public String getReportDate() 
+
+	public Date getReportDate() 
 	{
 		return reportDate;
 	}
-	
-	/**
-	 * 设置：交易码
-	 */
 	public void setTradeCode(String tradeCode) 
 	{
 		this.tradeCode = tradeCode;
 	}
-	
-	/**
-	 * 获取：交易码
-	 */
+
 	public String getTradeCode() 
 	{
 		return tradeCode;
 	}
-	
-	/**
-	 * 设置：交易笔数
-	 */
 	public void setTradeNum(Integer tradeNum) 
 	{
 		this.tradeNum = tradeNum;
 	}
-	
-	/**
-	 * 获取：交易笔数
-	 */
+
 	public Integer getTradeNum() 
 	{
 		return tradeNum;
 	}
-	
-	/**
-	 * 设置：交易金额
-	 */
 	public void setTradeSum(BigDecimal tradeSum) 
 	{
 		this.tradeSum = tradeSum;
 	}
-	
-	/**
-	 * 获取：交易金额
-	 */
+
 	public BigDecimal getTradeSum() 
 	{
 		return tradeSum;
 	}
-	
+	public void setRemark(String remark) 
+	{
+		this.remark = remark;
+	}
+
+	public String getRemark() 
+	{
+		return remark;
+	}
+	public void setStatus(String status) 
+	{
+		this.status = status;
+	}
+
+	public String getStatus() 
+	{
+		return status;
+	}
+	public void setCreateTime(Date createTime) 
+	{
+		this.createTime = createTime;
+	}
+
+	public Date getCreateTime() 
+	{
+		return createTime;
+	}
+	public void setUpdateTime(Date updateTime) 
+	{
+		this.updateTime = updateTime;
+	}
+
+	public Date getUpdateTime() 
+	{
+		return updateTime;
+	}
+
+    public String toString() {
+        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
+            .append("id", getId())
+            .append("reportDate", getReportDate())
+            .append("tradeCode", getTradeCode())
+            .append("tradeNum", getTradeNum())
+            .append("tradeSum", getTradeSum())
+            .append("remark", getRemark())
+            .append("status", getStatus())
+            .append("createTime", getCreateTime())
+            .append("updateTime", getUpdateTime())
+            .toString();
+    }
 }
