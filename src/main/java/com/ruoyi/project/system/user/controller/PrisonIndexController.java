d7b55f21c130e4411237e3b0e3fe1802d7333f10
肖长江 <caesar221050@163.com>
2019-03-25 21:09:49 +0800

    登录人员基本信息展示

--- src/main/java/com/ruoyi/project/system/user/controller/PrisonIndexController.java
+++ src/main/java/com/ruoyi/project/system/user/controller/PrisonIndexController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.ruoyi.framework.config.RuoYiConfig;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.project.business.person.domain.Person;
+import com.ruoyi.project.system.identity.domain.Identity;
+import com.ruoyi.project.system.identity.service.IIdentityService;
 import com.ruoyi.project.system.menu.service.IMenuService;
 
 /**
@@ -20,6 +22,9 @@ public class PrisonIndexController extends BaseController
 {
     @Autowired
     private IMenuService menuService;
+    
+    @Autowired
+    private IIdentityService identityService;
 
     @Autowired
     private RuoYiConfig prisonConfig;
@@ -30,10 +35,13 @@ public class PrisonIndexController extends BaseController
     {
         // 取身份信息
         Person user = getPerson();
+      //身份信息(从身份信息获取消费限额)
+  		Identity identity = identityService.selectIdentityById(user.getIdentityId());
         // 根据用户id取出菜单
 //        List<Menu> menus = menuService.selectMenusByUserId(user.getUserId());
 //        mmap.put("menus", menus);
         mmap.put("user", user);
+        mmap.put("identity", identity);
         mmap.put("copyrightYear", prisonConfig.getCopyrightYear());
         return "prisonIndex";
     }
